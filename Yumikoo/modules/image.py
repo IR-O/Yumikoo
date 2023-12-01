64faba8644869b46593cbacc288cb9e38c0dffe2
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2023-12-02 02:33:28 +0530

    Update image.py

--- Yumikoo/modules/image.py
+++ Yumikoo/modules/image.py
@@ -176,7 +176,7 @@ async def selectModel(_:Yumikoo,query:t.CallbackQuery):
         return await query.edit_message_text("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ @iro_x_support !!.")
     img_url = await ImageGeneration(modelId,promptData['prompt'])
     if img_url is None or img_url == 2 or img_url ==1:
-        return await query.edit_message_text("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ @DevsOops !!")
+        return await query.edit_message_text("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ @iro_x_support !!")
     elif img_url == 69:
         return await query.edit_message_text("ɴsғᴡ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ !")
     images = []
